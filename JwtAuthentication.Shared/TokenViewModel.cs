d5a40f726fc3b0272a1efbd57cd716b724c7899e
Karthik Anant <karthika25@gmail.com>
2018-11-22 23:16:55 -0500

    finished up the login workflow

--- JwtAuthentication.Shared/TokenViewModel.cs
+++ JwtAuthentication.Shared/TokenViewModel.cs
@@ -5,4 +5,9 @@
         public string Email { get; set; }
         public string Password { get; set; }
     }
+
+    public class TokenResponse
+    {
+        public string Token { get; set; }
+    }
 }
